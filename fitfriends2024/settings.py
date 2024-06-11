34d32b0a5ded322d234ee7fc8cb3dd5014473d05
abi kirkham <abbeykirkham9@icloud.com>
2024-06-11 08:40:51 +0000

    add forms.py to set up user authentication with django

--- fitfriends2024/settings.py
+++ fitfriends2024/settings.py
@@ -16,6 +16,10 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
+# User authentication
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGOUT_REDIRECT_URL = 'login'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')

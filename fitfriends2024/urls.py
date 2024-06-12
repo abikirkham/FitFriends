21405279795c6f26535d5d6bbf7494936d16755c
abi kirkham <abbeykirkham9@icloud.com>
2024-06-12 14:22:18 +0000

    Set register.html as home page

--- fitfriends2024/urls.py
+++ fitfriends2024/urls.py
@@ -20,7 +20,7 @@ from social import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('about/', views.about, name='about'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),

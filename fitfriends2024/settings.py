d160d0650cf442e342497a92942464f1b492ee71
abi kirkham <abbeykirkham9@icloud.com>
2024-06-18 13:26:57 +0000

    restructure istalled apps

--- fitfriends2024/settings.py
+++ fitfriends2024/settings.py
@@ -46,8 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'cloudinary',
     'social',
 ]
@@ -136,8 +136,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
